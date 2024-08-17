22a89359232ca7cb23eee541f54f0d7383413276
Yehor Malyshev <72669184+music-soul1-1@users.noreply.github.com>
2024-08-17 16:00:17 +0300

    fix(conf): change baseUrl in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'http://music-soul1-1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL ?? '/',
+  baseUrl: '/NureTimetableAPI.Docs/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
